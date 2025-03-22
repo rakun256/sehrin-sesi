176e21b01044fed255c977191893f1150147d939
rakun256 <emreuslu256@gmail.com>
2025-03-22 03:03:37 +0300

    Wrap RootNavigator with Redux Provider for state management

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from "react";
-import RootNavigator from "./navigation"
+import RootNavigator from "./navigation";
+import { Provider } from "react-redux";
 
 export default function App() {
-  return <RootNavigator />;
+  return (
+    <Provider store={store}>
+      <RootNavigator />
+    </Provider>
+  );
 }
