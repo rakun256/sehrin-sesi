d17e3afbb5d5860b66f8058b309bd2e8346c9dea
rakun256 <emreuslu256@gmail.com>
2025-03-22 03:51:15 +0300

    Implement login functionality with user authentication and token storage

--- screens/Auth/LoginScreen.js
+++ screens/Auth/LoginScreen.js
@@ -1,14 +1,67 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, Alert } from "react-native";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../../redux/slices/authSlice";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const LoginScreen = ({navigation}) => (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Login Screen</Text>
-        <Button
-            title="Click Here"
-            onPress={() => navigation.navigate("Register")}
-        />
-    </View>
-)
+const LoginScreen = ({ navigation }) => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const users = [
+      {
+        email: "admin@demo.com",
+        password: "123456",
+        name: "Admin Ecem",
+        role: "ADMIN",
+      },
+      {
+        email: "user@demo.com",
+        password: "123456",
+        name: "User Emre",
+        role: "USER",
+      },
+    ];
+
+    const matchedUser = users.find(
+      (user) => user.email === email && user.password === password
+    );
+
+    if (matchedUser) {
+      const token = "mock-token-123";
+      dispatch(loginSuccess({ user: matchedUser, token }));
+      await AsyncStorage.setItem("token", token);
+      Alert.alert("Giriş Başarılı", "Hoşgeldiniz " + matchedUser.name);
+    } else {
+      Alert.alert("Hata", "Email veya şifre hatalı.");
+    }
+  };
+
+  <View style={{ padding: 20, marginTop: 100 }}>
+    <Text style={{ fontSize: 24, marginBottom: 20 }}>Giriş Yap</Text>
+
+    <Text>Email</Text>
+    <TextInput
+      value={email}
+      onChangeText={setEmail}
+      autoCapitalize="none"
+      keyboardType="email-address"
+      style={{ borderBottomWidth: 1, marginBottom: 20 }}
+    />
+
+    <Text>Şifre</Text>
+    <TextInput
+      value={password}
+      onChangeText={setPassword}
+      secureTextEntry
+      style={{ borderBottomWidth: 1, marginBottom: 20 }}
+    />
+
+    <Button title="Giriş Yap" onPress={handleLogin} />
+    <Button title="Kayıt Ol" onPress={() => navigation.navigate("Register")} />
+  </View>;
+};
 
 export default LoginScreen;
