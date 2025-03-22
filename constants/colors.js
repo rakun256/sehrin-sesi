503904fc25ff12d5773e941f0ed6234618da8997
rakun256 <emreuslu256@gmail.com>
2025-03-22 05:51:50 +0300

    Add color constants for consistent theming across the application

---
+++ constants/colors.js
@@ -0,0 +1,22 @@
+const Colors = {
+    primary: '#A8D5BA',         
+    primaryLight: '#D9F2E6',    
+    primaryDark: '#6FBF73',     
+  
+    accent: '#C2EDCE',          
+    secondary: '#E6F5EA',       
+  
+    text: '#2F4F4F',            
+    mutedText: '#6E7F80',       
+  
+    white: '#FFFFFF',           
+    black: '#000000',           
+    error: '#FF6B6B',           
+    success: '#7ED6A4',         
+    background: '#F8FAF9',      
+  
+    transparent: 'transparent',
+  };
+  
+  export default Colors;
+  
