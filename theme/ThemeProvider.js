59ec90195a11bb8e2b836acd2a42f9d3b1115c10
rakun256 <emreuslu256@gmail.com>
2025-03-22 18:56:13 +0300

    Add expo-navigation-bar dependency and update ThemeProvider for navigation bar styling

--- theme/ThemeProvider.js
+++ theme/ThemeProvider.js
@@ -1,21 +1,23 @@
 import React, { useEffect } from "react";
-import { Platform, StatusBar, View } from "react-native";
+import { Platform, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as SystemUI from "expo-system-ui";
+import * as NavigationBar from "expo-navigation-bar";
+import { StatusBar } from "expo-status-bar";
 import Colors from "../constants/colors";
 
 const ThemeProvider = ({ children }) => {
   useEffect(() => {
     if (Platform.OS === "android") {
       SystemUI.setBackgroundColorAsync(Colors.background);
-      SystemUI.setNavigationBarColor(Colors.primaryDark);
-      SystemUI.setStatusBarColor(Colors.primaryDark);
+      NavigationBar.setBackgroundColorAsync(Colors.background);
+      NavigationBar.setButtonStyleAsync("dark"); 
     }
   }, []);
 
   return (
     <View style={{ flex: 1, backgroundColor: Colors.background }}>
-      <StatusBar barStyle="dark-content" backgroundColor={Colors.background} />
+      <StatusBar style="dark" backgroundColor={Colors.background} />
       <SafeAreaView style={{ flex: 1, backgroundColor: Colors.background }}>
         {children}
       </SafeAreaView>
