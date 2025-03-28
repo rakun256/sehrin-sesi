6e5715ccfd96cc9d2ab99efcdcaa153c19457628
rakun256 <emreuslu256@gmail.com>
2025-03-28 21:03:21 +0300

    Add HomeStack navigator with HomeScreen component

---
+++ navigation/stacks/HomeStack.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import  HomeScreen  from "../../screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+const HomeStack = () => {
+    <Stack.Navigator>
+        <Stack.Screen 
+        name="Home"
+        component={HomeScreen}
+        options={{
+            title: "Harita"
+        }}
+        />
+    </Stack.Navigator>
+}
